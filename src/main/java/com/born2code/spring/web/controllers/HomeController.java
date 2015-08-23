196cc06f640f2c929582e5fc14d72cb18dfd43c0
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-08-23 12:47:42 +0600

    Completed - 12-03 Using Logging

--- src/main/java/com/born2code/spring/web/controllers/HomeController.java
+++ src/main/java/com/born2code/spring/web/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.born2code.spring.web.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +9,11 @@ import java.lang.String;
 @Controller
 public class HomeController {
 
+    private static Logger logger = Logger.getLogger(HomeController.class);
+
     @RequestMapping("/")
     public String showHome() {
+        logger.info("Showing home ...");
         return "home";
     }
 }
